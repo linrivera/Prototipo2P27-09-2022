87081c2a3a0254a6225f7d743925d4b59bd58d8a
linrivera <linriveracifu@gmail.com>
2022-09-27 13:23:34 -0600

    Subiendo mantenimiento

--- CapaVista/frmNomina.Designer.cs
+++ CapaVista/frmNomina.Designer.cs
@@ -448,6 +448,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(1145, 450);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox2);
